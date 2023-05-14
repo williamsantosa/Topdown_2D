a95e3d85c4ddb5955403e3668faa2450c7f93a30
Will <williamwsantosa@gmail.com>
2023-05-13 21:31:41 -0700

    Updated with animation moving up down left right, but no transition between idle and walk animations yet 39:09

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -1,6 +1,14 @@
 extends CharacterBody2D
 
 @export var move_speed : float = 100
+@export var starting_direction : Vector2 = Vector2(0, 1)
+
+# parameters/Idle/blend_position
+
+@onready var animation_tree = $AnimationTree
+
+func _ready():
+	update_animation_parameters(starting_direction)
 
 func _physics_process(_delta):
 	# Get input direction
@@ -9,8 +17,15 @@ func _physics_process(_delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
+	update_animation_parameters(input_direction)
 	# Update velocity
 	velocity = input_direction * move_speed
 	
 	# Move and Slide function uses velocity of character body to move character on map
 	move_and_slide()
+
+func update_animation_parameters(move_input : Vector2):
+	# Don't change animation parameters if there is no move input
+	if (move_input != Vector2.ZERO):
+		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Idle/blend_position", move_input)

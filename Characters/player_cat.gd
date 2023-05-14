b8dd459a3efb61499f303baf5e8a924701555fa0
Will <williamwsantosa@gmail.com>
2023-05-13 21:00:50 -0700

    29:06 on tutorial, animations created and named

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -1,5 +1,7 @@
 extends CharacterBody2D
 
+@export var move_speed : float = 100
+
 func _physics_process(_delta):
 	# Get input direction
 	var input_direction = Vector2(
@@ -7,8 +9,8 @@ func _physics_process(_delta):
 		Input.get_action_strength("down") - Input.get_action_strength("up")
 	)
 	
-	print(input_direction)
-	
 	# Update velocity
+	velocity = input_direction * move_speed
 	
 	# Move and Slide function uses velocity of character body to move character on map
+	move_and_slide()

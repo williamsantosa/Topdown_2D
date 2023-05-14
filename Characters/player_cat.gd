3a4612803bef7a562d6a60f42c7c4c352832958d
Will <williamwsantosa@gmail.com>
2023-05-13 21:49:51 -0700

    46:37 working on tilemap right now, just finished layering

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -6,6 +6,7 @@ extends CharacterBody2D
 # parameters/Idle/blend_position
 
 @onready var animation_tree = $AnimationTree
+@onready var state_machine = animation_tree.get("parameters/playback")
 
 func _ready():
 	update_animation_parameters(starting_direction)
@@ -23,9 +24,19 @@ func _physics_process(_delta):
 	
 	# Move and Slide function uses velocity of character body to move character on map
 	move_and_slide()
+	
+	pick_new_state()
 
 func update_animation_parameters(move_input : Vector2):
 	# Don't change animation parameters if there is no move input
 	if (move_input != Vector2.ZERO):
 		animation_tree.set("parameters/Walk/blend_position", move_input)
 		animation_tree.set("parameters/Idle/blend_position", move_input)
+
+# Choose state based on what is happening with the player
+func pick_new_state():
+	if (velocity != Vector2.ZERO):
+		state_machine.travel("Walk")
+	else:
+		state_machine.travel("Idle")
+
